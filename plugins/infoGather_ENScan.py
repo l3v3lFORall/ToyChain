047abe37cf58846c401742764503fc2b1d38c2cd
AllMighty <3311951946@qq.com>
2023-04-20 14:32:44 +0800

    完善ENScan插件

--- plugins/infoGather_ENScan.py
+++ plugins/infoGather_ENScan.py
@@ -13,9 +13,49 @@ class customPlugin(_up.Plugin):
             "other": []
         }
         
-    def extractData(self):
-        data = {}
-        return data
+    def extractData(self, path):
+        """提取一些工具输出的xlsx文件中的通用信息
+
+        Returns:
+            _type_: _description_
+        """
+        
+        def extractICP():
+            result = []
+            ws = resultXlsx['ICP备案']
+            if ws["C1"].value != "域名":
+                raise Exception("ICP备案的结果不符合预期")
+            for row in ws[f'C2:C{ws.max_row}']:
+                for cell in row:
+                    result.append(cell.value)
+            return result
+        def extractAPP():
+            result = []
+            ws = resultXlsx['APP']
+            if ws["A1"].value != "名称":
+                raise Exception("APP信息的结果不符合预期")
+            for row in ws[f'A2:A{ws.max_row}']:
+                for cell in row:
+                    result.append(cell.value)
+            return result            
+        import os
+        fileList = os.listdir(path)
+        from openpyxl import load_workbook
+        for _fl in fileList:
+            resultXlsx = load_workbook(
+                os.path.join(path, _fl)
+            )
+            try:
+                self.pluginResult["subdomain"] = extractICP()
+                self.pluginResult["app"] = extractAPP()
+            except Exception as e:
+                pInfo(f"|--插件提取数据出错：{resultXlsx}")
+                pInfo(f"|--{e}")
+                continue
+        
+
+    
+    
     def run(self, *args, **kwargs):
         """
         run 运行插件，Based on https://github.com/wgpsec/ENScan_GO
@@ -24,9 +64,10 @@ class customPlugin(_up.Plugin):
             kwargs["config"]: 导入的配置
         
         Returns:
-            _type_: _description_
+            dict: 提取子域名、APP数据，保存导出文件的路径
         """
-        outPath = "out/infoGather_ENScan/"
+        import time
+        outPath = f"out/infoGather_ENScan/{int(round(time.time() * 1000))}"
         myProxy = "https://" + kwargs["config"]["proxy"]["https"]
         cmd = kwargs["config"]["cmd"]
         # cmd: "ENScanGO.exe -is-merge -branch -o {} -n {}"
@@ -45,5 +86,7 @@ class customPlugin(_up.Plugin):
         output, error_msgs = res.communicate()
         print(output.decode(encoding='utf-8'))
         
+        self.extractData(outPath)
         self.pluginResult["other"].append(outPath)
+        pInfo(f"|--获取子域名{len(self.pluginResult['subdomain'])}条，APP信息{len(self.pluginResult['app'])}条；导出文件到{self.pluginResult['other']}")
         return self.pluginResult
